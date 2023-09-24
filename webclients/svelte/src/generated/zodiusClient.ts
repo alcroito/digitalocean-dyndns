b99bbf3f890dfee35f4ae1ec0207b747071ed0fb
alcroito <alcroito.code@pm.me>
2023-09-24 15:33:35 +0200

    chore(web): Regenerate open api zodius client

--- webclients/svelte/src/generated/zodiusClient.ts
+++ webclients/svelte/src/generated/zodiusClient.ts
@@ -1,15 +1,17 @@
 import { makeApi, Zodios, type ZodiosOptions } from '@zodios/core';
 import { z } from 'zod';
 
-const DomainRecordIpChange = z.object({
-	attempt_date: z.string(),
-	domain_record_id: z.number().int(),
-	id: z.number().int(),
-	name: z.string(),
-	set_ip: z.string(),
-	success: z.boolean()
-});
-const DomainRecordIpChanges = z.object({ changes: z.array(DomainRecordIpChange) });
+const DomainRecordIpChange = z
+	.object({
+		attempt_date: z.string(),
+		domain_record_id: z.number().int(),
+		id: z.number().int(),
+		name: z.string(),
+		set_ip: z.string(),
+		success: z.boolean()
+	})
+	.passthrough();
+const DomainRecordIpChanges = z.object({ changes: z.array(DomainRecordIpChange) }).passthrough();
 
 export const schemas = {
 	DomainRecordIpChange,
@@ -20,26 +22,28 @@ const endpoints = makeApi([
 	{
 		method: 'get',
 		path: '/api/v1/domain_record_ip_changes',
+		alias: 'getApiv1domain_record_ip_changes',
 		description: `List all recent domain record ip changes`,
 		requestFormat: 'json',
 		response: DomainRecordIpChanges,
 		errors: [
 			{
 				status: 'default',
-				schema: z.object({ GenericError: z.string() })
+				schema: z.object({ GenericError: z.string() }).passthrough()
 			}
 		]
 	},
 	{
 		method: 'get',
 		path: '/docs/',
+		alias: 'getDocs',
 		description: `This documentation page.`,
 		requestFormat: 'json',
 		response: z.void(),
 		errors: [
 			{
 				status: 'default',
-				schema: z.object({ GenericError: z.string() })
+				schema: z.object({ GenericError: z.string() }).passthrough()
 			}
 		]
 	}
